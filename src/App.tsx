5a19a1fd0f72b6f60ea38da25325682a00acba9f
Aleksandar Pavlovic <apavlovic@Aleksandars-MacBook-Pro.local>
2022-07-10 03:56:44 +0200

    Add ol with basic map and fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { mapService } from './services/map';
+
 import "./App.css";
 
-function App() {
-    return (
-        <h1>Test</h1>
-    );
+class App extends React.Component {
+    componentDidMount() {
+        const map = mapService.getMap();
+        map.setTarget('map');
+    }
+
+    render() {
+        return (
+            <>
+                <div id="map"></div>
+            </>
+        );
+    }
 }
 
 export default App;
