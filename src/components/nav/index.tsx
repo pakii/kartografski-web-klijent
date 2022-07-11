cfb53115760f9d0f7f5f0406f09a2f8bdc323fbd
Aleksandar Pavlovic <apavlovic@Aleksandars-MacBook-Pro.local>
2022-07-11 15:52:04 +0200

    Updates

--- src/components/nav/index.tsx
+++
@@ -1,9 +0,0 @@
-import { mapService } from '../../shared/map-service';
-
-export const Nav = () => {
-    const map = mapService.getMap();
-    map.getView().setZoom(5);
-    return (
-        <h1>Test nav</h1>
-    )
-}
